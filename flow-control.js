1f6c68ea1c763bf34825954fde98383c664d4418
rihan <dikshabelwal0@gmail.com>
2019-01-31 09:42:48 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,34 @@
 function basicTeenager(age) {
+  if (13<=age || age<=19)
+  { console.log("You are a teenager!")  }
 
 }
 
 function teenager(age) {
-
+  if (13<=age || age<=19)
+  { console.log("You are a teenager!")  }
+  else { console.log("You are not a teenager")}
 }
 
 function ageChecker(age) {
-
+if (13<=age && age<=19) {console.log("You are a teenager!")}
+else if (age<=12) {console.log("You are a kid")}
+else if (20<=age) {console.log("You are a grownup")}
 }
 
 function ternaryTeenager(age) {
-
+(13<=age && age<=19) ? return "You are a teenager" : return "You are not a teenager"
 }
 
 function switchAge(age) {
-
+switch(age)
+{case 13<=age && age<=19 : return "You are a teenager"
+                             break
+default : return "You have an age"
+                break }
 }
+basicTeenager(12);
+teenager(19);
+ageChecker(22);
+ternaryTeenager(65);
+switchage(7);
